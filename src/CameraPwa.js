dc08a51211c08e4ade1a082e35dd223077778d5b
regola-Tacens <regolas-tacens@outlook.fr>
2022-09-11 21:06:10 +0100

    added playsinline and muted

--- src/CameraPwa.js
+++ src/CameraPwa.js
@@ -11,7 +11,8 @@ const CameraPwa = () => {
       facingMode: "environment",
       width: { min: 1024, ideal: 1280, max: 1920 },
       height: { min: 776, ideal: 720, max: 1080 }
-   }
+    }, 
+    audio: false
    }).then(stream => {
     video = videoRef.current;
     if(video) {
@@ -30,7 +31,7 @@ const CameraPwa = () => {
     <div style={{position: 'relative'}}>
       <div style={{overflow: 'hidden', margin: '0 auto'}}>
         <Masque />
-        <video style={{zIndex: 2, overflow:"hidden", height:'100vh', margin: '0 auto', position:'relative'}} ref={videoRef} autoPlay />
+        <video style={{zIndex: 2, overflow:"hidden", height:'100vh', margin: '0 auto', position:'relative'}} ref={videoRef} autoPlay muted playsinline />
       </div>
       <canvas ref={canvasRef} style={{display: 'none'}}/>
       <button style={{position: 'absolute',bottom:'1rem', zIndex:'150', marginLeft:'50%', transform: 'translateX(-50%)', borderRadius:'50%', width:'80px', height:'80px', border:"none"}} onClick={handleTakePhoto}>Shoot !</button>
--- src/Masque.js
+++ src/Masque.js
@@ -2,7 +2,7 @@
 const Masque = () => {
   
   return<div style={{position: 'absolute', zIndex: 50}}>
-    yoooooo
+    added muted and playsinline
     {/* <img style={{width:'100%'}} src={mask} alt="masque"/> */}
     <svg width="" height="100vh" viewBox="0 0 232 351" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path fill-rule="evenodd" clip-rule="evenodd" d="M232 0H0V351H232V0ZM100 72H128L152 123V253L106 305L28 189L100 72Z" fill="#F21818" fill-opacity="0.6"/>
