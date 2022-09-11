8ebcc3a2da1bf260b5734ff535e12d48ac872309
regola-Tacens <regolas-tacens@outlook.fr>
2022-09-11 18:42:33 +0100

    test3

--- src/CameraPwa.js
+++ src/CameraPwa.js
@@ -7,7 +7,11 @@ const CameraPwa = () => {
   let video = videoRef.current
 
   navigator.mediaDevices.getUserMedia({
-    video: { facingMode: { exact: "environment" } }
+    video: { 
+      facingMode: "environment",
+      width: { min: 1024, ideal: 1280, max: 1920 },
+      height: { min: 776, ideal: 720, max: 1080 }
+   }
    }).then(stream => {
     video = videoRef.current;
     if(video) {
