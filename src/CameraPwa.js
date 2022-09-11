4d22a5e92339528852c811b496c337ed70fe8bdd
regola-Tacens <regolas-tacens@outlook.fr>
2022-09-11 19:11:47 +0100

    ll

--- src/CameraPwa.js
+++ src/CameraPwa.js
@@ -30,7 +30,7 @@ const CameraPwa = () => {
     <div style={{position: 'relative'}}>
       <div style={{overflow: 'hidden', margin: '0 auto'}}>
         <Masque />
-        <video style={{zIndex: 2, overflow:"hidden", height:'100vh', margin: '0 auto', position:'relative', transform:'translateX(-50%)'}} ref={videoRef} autoPlay />
+        <video style={{zIndex: 2, overflow:"hidden", height:'100vh', margin: '0 auto', position:'relative'}} ref={videoRef} autoPlay />
       </div>
       <canvas ref={canvasRef} style={{display: 'none'}}/>
       <button style={{position: 'absolute',bottom:'1rem', zIndex:'150', marginLeft:'50%', transform: 'translateX(-50%)', borderRadius:'50%', width:'80px', height:'80px', border:"none"}} onClick={handleTakePhoto}>Shoot !</button>
